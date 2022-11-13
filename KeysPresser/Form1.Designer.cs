c8d4ca4a3645efe2b2d8c83473bef5d01ebbf3ab
robezonetv <78911491+robezonetv@users.noreply.github.com>
2022-11-13 15:17:30 -0600

    initial commit

---
+++ KeysPresser/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace KeysPresser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.btn_Refresh = new System.Windows.Forms.Button();
+            this.timeout = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.timeout)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(44, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(299, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose an application:";
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(187, 52);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 2;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(268, 52);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_Stop.TabIndex = 3;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // btn_Refresh
+            // 
+            this.btn_Refresh.Image = global::KeysPresser.Properties.Resources._14146_arrows_refresh_reload_icon;
+            this.btn_Refresh.Location = new System.Drawing.Point(12, 23);
+            this.btn_Refresh.Name = "btn_Refresh";
+            this.btn_Refresh.Size = new System.Drawing.Size(26, 24);
+            this.btn_Refresh.TabIndex = 4;
+            this.btn_Refresh.UseVisualStyleBackColor = true;
+            this.btn_Refresh.Click += new System.EventHandler(this.btn_Refresh_Click);
+            // 
+            // timeout
+            // 
+            this.timeout.Location = new System.Drawing.Point(12, 55);
+            this.timeout.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.timeout.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.timeout.Name = "timeout";
+            this.timeout.Size = new System.Drawing.Size(48, 20);
+            this.timeout.TabIndex = 5;
+            this.timeout.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "ms wait";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 87);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.timeout);
+            this.Controls.Add(this.btn_Refresh);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Silkroad auto keypresser";
+            ((System.ComponentModel.ISupportInitialize)(this.timeout)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Stop;
+        private System.Windows.Forms.Button btn_Refresh;
+        private System.Windows.Forms.NumericUpDown timeout;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ KeysPresser/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace KeysPresser
+{
+    public partial class Form1 : Form
+    {
+
+        private int status = 0;
+
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(IntPtr hWnd);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Apps
+        {
+            public string name { get; set; }
+            public int pid { get; set; }
+        }
+
+        public void UpdateComboBox(string data)
+        {
+            comboBox1.Items.Add(data);
+        }
+        
+        private async void btn_Start_Click(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+
+            this.comboBox1.Enabled = false;
+            this.btn_Start.Enabled = false;
+            this.btn_Refresh.Enabled = false;
+            this.timeout.Enabled = false;
+
+            this.status = 0;
+            int pid = ((Apps)this.comboBox1.SelectedItem).pid;
+            Process proc = Process.GetProcessById(pid);
+            int j;
+            while (true)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    if (i == 10)
+                    {
+                        j = 0;
+                    }
+                    else
+                    {
+                        j = i;
+                    }
+
+                    if (this.status == 0)
+                    {
+                        SetForegroundWindow(proc.MainWindowHandle);
+                        SendKeys.SendWait("{" + j.ToString() + "}");
+                        await Task.Delay((int)this.timeout.Value);
+                        //Thread.Sleep((int) this.timeout.Value);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (this.status == 1)
+                {
+                    break;
+                }
+            }
+        }
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            this.status = 1;
+            this.comboBox1.Enabled = true;
+            this.btn_Start.Enabled = true;
+            this.btn_Refresh.Enabled = true;
+            this.timeout.Enabled = true;
+        }
+
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+            this.comboBox1.DataSource = null;
+            //this.comboBox1.Items.Clear();
+            var dataSource = new List<Apps>();
+
+            Process[] processlist = Process.GetProcesses();
+            foreach (Process process in processlist)
+            {
+                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    dataSource.Add(new Apps() { name = process.Id.ToString() + " ["+process.ProcessName+"] "+process.MainWindowTitle, pid = process.Id });
+                    //this.UpdateComboBox(process.ProcessName + " (" + process.MainWindowTitle + ")");
+                }
+            }
+
+            this.comboBox1.DataSource = dataSource;
+            this.comboBox1.DisplayMember = "name";
+            this.comboBox1.ValueMember = "pid";
+
+        }
+    }
+}
